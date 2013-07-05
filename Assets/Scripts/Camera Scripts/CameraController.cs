118741a24ab320ae834d3c5302bb8f4084f36cd3
Justin Smith <jds0102@gmail.com>
2013-07-05 16:02:00 -0500

    Stopped camera at edge of screen, somewhat hard coded implementation

--- Assets/Scripts/Camera Scripts/CameraController.cs
+++ Assets/Scripts/Camera Scripts/CameraController.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     private float m_normalFOV;
     private PlayerCharacter m_player;
@@ -9,55 +10,60 @@ public class CameraController : MonoBehaviour {
     private Vignetting m_vignette;
 
     public static CameraController s_singleton;
+    private GameObject floor;
+    private float maxYFromCenter, maxXFromCenter;
 
-
-	// Use this for initialization
-	void Start () {
-        if (!s_singleton)
-        {
+    // Use this for initialization
+    void Start()
+    {
+        if (!s_singleton) {
             s_singleton = this;
         }
         m_player = PlayerCharacter.s_singleton;
         m_normalFOV = Camera.main.fov;
         m_motionBlur = gameObject.GetComponent<MotionBlur>();
         m_vignette = gameObject.GetComponent<Vignetting>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Camera.main.fov > m_normalFOV)
-        {
+        floor = GameObject.Find("Level").transform.FindChild("Floor").gameObject;
+        maxXFromCenter = floor.transform.GetComponent<BoxCollider>().bounds.extents.x * .575f;
+        maxYFromCenter = floor.transform.GetComponent<BoxCollider>().bounds.extents.z * .505f;
+        Debug.Log(floor.transform.GetComponent<BoxCollider>().bounds.extents);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Camera.main.fov > m_normalFOV) {
             Camera.main.fov--;
         }
 
         m_motionBlur.blurAmount = (float)m_player.m_heartRate / (float)m_player.GetMaxHeartRate();
-       
-        m_vignette.intensity = (10.0f)*((float)(90 + m_player.GetMinHeartRate() - m_player.m_heartRate)) / (90 + m_player.GetMinHeartRate());
-        
+
+        m_vignette.intensity = (10.0f) * ((float)(90 + m_player.GetMinHeartRate() - m_player.m_heartRate)) / (90 + m_player.GetMinHeartRate());
+
 
         m_vignette.chromaticAberration = m_vignette.intensity / 1.4f;
         m_vignette.blur = m_vignette.intensity / 10.0f;
 
-        if (m_player.m_heartRate > m_player.GetMaxHeartRate() * 6.0f / 13.0f)
-        {
+        if (m_player.m_heartRate > m_player.GetMaxHeartRate() * 6.0f / 13.0f) {
             m_motionBlur.enabled = true;
             m_vignette.enabled = false;
-        }
-        else if (m_player.m_heartRate < 90)
-        {
+        } else if (m_player.m_heartRate < 90) {
             m_motionBlur.enabled = false;
             m_vignette.enabled = true; ;
-        }else
-        {
+        } else {
             m_motionBlur.enabled = false;
             m_vignette.enabled = false;
         }
 
         Vector3 pos = this.transform.position;
-        pos.x = m_player.transform.position.x;
-        pos.z = m_player.transform.position.z;
+        if (Mathf.Abs(m_player.transform.position.x - floor.transform.position.x) < maxXFromCenter) {
+            pos.x = m_player.transform.position.x;
+        }
+        if (Mathf.Abs(m_player.transform.position.z - floor.transform.position.z) < maxYFromCenter) {
+            pos.z = m_player.transform.position.z;
+        }
         this.transform.position = pos;
-	}
+    }
 
 
     /// <summary>
