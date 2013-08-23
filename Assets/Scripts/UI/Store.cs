dcdacb3c45b887c331de1108cdc6ffa0f04e1a2e
Emanuel Rosu <rosu.emanuel@gmail.com>
2013-08-23 13:14:22 -0500

    better programming for store, move to toggles over buttons

--- Assets/Scripts/UI/Store.cs
+++ Assets/Scripts/UI/Store.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Store : MonoBehaviour {
 	private static Store m_singleton;
@@ -20,12 +21,16 @@ public class Store : MonoBehaviour {
 
     private Vector2 m_scrollPos = Vector2.zero;
 	private bool showRight;
+    //private List<Weapon> m_storeWeapons;
 	
 	void Start() {
 		if (m_singleton == null) {
 			m_singleton = this;
 		}
-		
+        //m_storeWeapons = new Dictionary<Weapon,bool>();
+        //foreach (Weapon selected in WeaponManager.m_singleton.m_weapons) {
+        //    m_storeWeapons.Add(selected, false);
+        //}
 		m_storeBounds = new Rect(Screen.width * .10f, Screen.height * .10f, Screen.width * .8f, Screen.height * .8f);
         m_bottomButtonLeftBounds = new Rect(m_storeBounds.width * .01f, m_storeBounds.height - m_storeBounds.height * .01f - m_storeBounds.height * .1f, m_storeBounds.width * .97f / 2, m_storeBounds.height * .1f);
         m_bottomButtonRightBounds = new Rect(m_bottomButtonLeftBounds.xMax + m_storeBounds.width * .01f, m_bottomButtonLeftBounds.y, m_bottomButtonLeftBounds.width, m_bottomButtonLeftBounds.height);
@@ -50,7 +55,16 @@ public class Store : MonoBehaviour {
 
         GUI.BeginGroup(m_itemListBounds);
 
-        if (GUI.Button(new Rect(0, 10, m_itemListBounds.width, 20), "A Really Sick Item", m_listStyle)) {
+        float y = 10;
+        foreach (Weapon selected in WeaponManager.m_singleton.m_weapons) {
+            //bool value = m_storeWeapons.TryGetValue(selected, out value);
+            selected.m_selectedInStore = GUI.Toggle(new Rect(0, y, m_itemListBounds.width, 20), selected.m_selectedInStore, selected.name, m_listStyle);
+            Debug.Log("New Value: " + selected.m_selectedInStore);
+            y += 30;
+            
+        }
+
+        /*if (GUI.Button(new Rect(0, 10, m_itemListBounds.width, 20), "A Really Sick Item", m_listStyle)) {
 			Debug.Log("Item Selected");
 			showRight = true;
 		}
@@ -63,9 +77,17 @@ public class Store : MonoBehaviour {
         GUI.Button(new Rect(0, 220, m_itemListBounds.width, 20), "A Really Sick Item", m_listStyle);
         GUI.Button(new Rect(0, 250, m_itemListBounds.width, 20), "A Really Sick Item", m_listStyle);
         GUI.Button(new Rect(0, 280, m_itemListBounds.width, 20), "A Really Sick Item", m_listStyle);
-        GUI.Button(new Rect(0, 310, m_itemListBounds.width, 20), "A Really Sick Item", m_listStyle);
+        GUI.Button(new Rect(0, 310, m_itemListBounds.width, 20), "A Really Sick Item", m_listStyle);*/
         GUI.EndGroup();
-		
+
+        foreach (Weapon selected in WeaponManager.m_singleton.m_weapons) {
+            if (selected.m_selectedInStore) {
+                if (GUI.Button(m_selectedItemBounds, "Buy", m_buttonStyle) && GameManager.s_singleton.m_score >= 100) {
+                    WeaponManager.m_singleton.m_ownedWeapons.Add(WeaponManager.GetWeapon("TriShot"));
+                    GameManager.s_singleton.m_score -= 100;
+                }
+            }
+        }
 		if (showRight) { 
 			if (GUI.Button(m_selectedItemBounds, "Buy", m_buttonStyle) && GameManager.s_singleton.m_score >= 100) {
 				WeaponManager.m_singleton.m_ownedWeapons.Add(WeaponManager.GetWeapon("TriShot"));
