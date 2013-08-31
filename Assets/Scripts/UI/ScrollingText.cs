f0d1fbd45a791101f2e26a51e1f2cf6973b2b7e5
MaritalWheat <rosu.emanuel@gmail.com>
2013-08-31 14:50:45 -0700

    Art Art Art (& some UI behavior changes)

--- Assets/Scripts/UI/ScrollingText.cs
+++ Assets/Scripts/UI/ScrollingText.cs
@@ -9,11 +9,13 @@ public class ScrollingText : MonoBehaviour {
     private Rect m_notificationRect;
     private float m_speed;
     private bool m_display = false;
+	private float m_fontSize;
+	private float m_duration;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		//m_duration = 5f;
 	}
 	
 	// Update is called once per frame
@@ -24,11 +26,23 @@ public class ScrollingText : MonoBehaviour {
         }
         if (m_display)
         {
-            if (m_notificationRect.x + m_notificationRect.width < 0)
+            if (m_duration < 0)
             {
                 Destroy(this.gameObject);
             }
-            m_notificationRect.x -= (m_speed);
+            //m_notificationRect.x -= (m_speed);
+			//m_notificationRect.width += m_speed;
+			if (m_duration > .5f) {
+				m_fontSize += m_speed * 5 * Time.deltaTime;
+			} else {
+				m_fontSize -= m_speed * 5 * Time.deltaTime;
+			}
+			
+			m_duration -= Time.deltaTime;
+			//} else { 
+			//	m_fontSize -= m_speed;
+			//}
+			//m_notificationRect.height += m_speed;
         }
 	}
 
@@ -40,6 +54,8 @@ public class ScrollingText : MonoBehaviour {
         }
         if (m_display)
         {
+			if (m_fontSize <= 1) m_fontSize = 1;
+			m_style.fontSize = (int)m_fontSize;
             GUI.Label(m_notificationRect, m_message, m_style);
         }
     }
@@ -47,9 +63,13 @@ public class ScrollingText : MonoBehaviour {
     public void Display(string message, float speed, GUIStyle style ) {
         m_style = style;
         m_message = message;
+		
         Vector2 size = m_style.CalcSize(new GUIContent(message));
-        m_notificationRect = new Rect(Screen.width, Screen.height/2-(size.y/2), size.x, size.y);
+        m_notificationRect = new Rect(Screen.width / 2 - size.x / 2, Screen.height/2-(size.y/2), size.x, size.y);
+		m_style.fontSize = 1;
+		m_fontSize = m_style.fontSize;
         m_speed = speed;
+		m_duration = 1f;
         m_display = true;
     }
 
