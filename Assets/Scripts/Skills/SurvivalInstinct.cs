240be8197f191764b14735ae70f9f16c038c63f5
MaritalWheat <rosu.emanuel@gmail.com>
2013-11-27 00:07:04 -0600

    updated survival instinct to be timed skill

--- Assets/Scripts/Skills/SurvivalInstinct.cs
+++ Assets/Scripts/Skills/SurvivalInstinct.cs
@@ -4,7 +4,7 @@ using System.Collections;
 //passive ability that activates if health drops below a certain percentage, increasing player speed while
 //slowing down enemies
 
-public class SurvivalInstinct : Skill {
+public class SurvivalInstinct : TimedSkill {
 	private bool m_activated;
 	
 	public override void Execute() 
@@ -15,14 +15,14 @@ public class SurvivalInstinct : Skill {
 				EnemyManager.Enemies[i].m_speed = EnemyManager.Enemies[i].GetBaseSpeed() / 3;
 			}
 			PlayerCharacter.s_singleton.m_maxCharacterSpeed += 50.0f;
-			//SkillManager.m_singleton.StartCoroutine("Activated");
+
+			StartTimer();
 		} else {
 			m_activated = false;
 			for (int i = 0; i < EnemyManager.Enemies.Count; i++) {
 				EnemyManager.Enemies[i].m_speed = EnemyManager.Enemies[i].GetBaseSpeed();
 			}
 			PlayerCharacter.s_singleton.m_maxCharacterSpeed -= 50.0f;
-			//SkillManager.m_singleton.StopCoroutine("Activated");
 		}
 	}
 }
