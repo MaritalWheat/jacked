034f2e2461ff8648dc14ef46c075fde9c454d488
MaritalWheat <rosu.emanuel@gmail.com>
2013-11-27 00:05:08 -0600

    updated stabilizer w/ timer, added TimedSkill superclass

--- Assets/Scripts/Skills/Stabilizer.cs
+++ Assets/Scripts/Skills/Stabilizer.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 //temporarily freezes heart rate to current
-public class Stabilizer : Skill {
+public class Stabilizer : TimedSkill {
 	private bool m_activated;
 	
 	public override void Execute() 
@@ -10,6 +10,7 @@ public class Stabilizer : Skill {
 		if (!m_activated) {
 			m_activated = true;
 			PlayerCharacter.IgnoreBeatCycle = true;
+			StartTimer();
 		} else {
 			m_activated = false;
 			PlayerCharacter.IgnoreBeatCycle = false;
---
+++ Assets/Scripts/Skills/TimedSkill.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class TimedSkill : Skill {
+	[HideInInspector]
+	public float m_timer = 0.0f;
+	public float m_duration;
+
+	public virtual int Timer(int arg) {
+		m_timer += Time.deltaTime;
+		Debug.Log(m_timer);
+		if (m_timer > m_duration) {
+			Execute();
+			return 1;
+		}
+		return 0;
+	}
+
+	public virtual void StartTimer() {
+		m_timer = 0.0f;
+		Func<int, int> executable = Timer;
+		CoroutineHandler.StartCoroutine(executable);
+	}
+}
